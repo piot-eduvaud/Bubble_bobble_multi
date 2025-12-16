366d1cc7a7a1f4cb811f056f76e1adee30703873
piot-eduvaud <pascal.piot@eduvaud.ch>
2025-12-16 23:31:27 +0100

    chore: remove debug code and prepare for production release

--- server.js
+++ server.js
@@ -795,7 +795,6 @@ io.on('connection', (socket) => {
             items: currentRoom.items
         });
         socket.emit('highscores', highScores);
-        socket.emit('debug_info', { isMobile: player.isMobile }); // DEBUG Verification
     });
 
     socket.on('request_highscores', () => {
